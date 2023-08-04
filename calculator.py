b261e99681fc62c2705257604e1ce188929c5418
eduardgheorghe <gheorgheeduardemil9@gmail.com>
2023-08-04 14:47:03 +0300

    added functionality on buttons

--- calculator.py
+++ calculator.py
@@ -1,13 +1,18 @@
 from tkinter import *
+from functools import partial
 
 def close_app():
     window.destroy()
+
+def button_click(value):
+    expression_field_value.set(expression_field_value.get() + str(value))
 
 if __name__ == "__main__":
     window = Tk()
     window.title("PyCalc")
     window.minsize(300, 250)
 
+    expression_field_value = StringVar()
     expression_field = Entry(window, width=30)
     expression_field.grid(row=0, column=0, columnspan=4, sticky="nw")
 
