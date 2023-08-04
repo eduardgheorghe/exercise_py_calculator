674001c27c32d0b8e5a09333669e7ab9c5fe3ac1
eduardgheorghe <gheorgheeduardemil9@gmail.com>
2023-08-04 14:38:27 +0300

    removed buttons overlap on expression field

--- calculator.py
+++ calculator.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     window.minsize(300, 250)
 
     expression_field = Entry(window, width=30)
-    expression_field.grid(row=0, column=0, columnspan=4, sticky="ew")
+    expression_field.grid(row=0, column=0, columnspan=4, sticky="nw")
 
 
     for i in range(1, 10):
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         button = Button(window, text=str(i), height=3, width=3, borderwidth=1)
 
         # Configure the button to expand horizontally (sticky="ew")
-        button.grid(row=(i-1)//3, column=(i-1)%3, sticky="ew")
+        button.grid(row=(i+2)//3, column=(i-1)%3, sticky="ew")
 
         # Bind the click event to the button_click function
         button.config(command=lambda num=i: button_click(num))
